090ee7b8d95e1d8d3686644bded1f3fe6800256f
Max Chistiakov <chistiakovmax@gmail.com>
2024-09-08 01:23:21 +0200

    Now API works through the server

--- src/components/Signs.jsx
+++ src/components/Signs.jsx
@@ -15,8 +15,8 @@ const Signs = () => {
     useEffect(() => {
         const fetchData = async (sign) => {
             try {
-                const response = await axios.get(`https://horoscope-app-api.vercel.app/api/v1/get-horoscope/daily?sign=${sign}&day=TODAY`);
-                setData(response.data.data.horoscope_data);
+                const response = await axios.get(`https://pure-server.onrender.com/horoscope?sign=${sign}`);
+                setData(response.data);
             } catch (error) {
                 console.error('Ошибка запроса', error)
                 setData('Ошибка загрузки данных')
