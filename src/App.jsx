93092ae43c145de9c2a479d65a2a05dca1e3a68f
Max Chistiakov <chistiakovmax@gmail.com>
2024-09-08 03:53:39 +0200

    add telegram button

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
 import './App.css'
 import Zodiacs from './components/Zodiacs';
 
+const tg = window.Telegram.WebApp
+const onClose = () => {
+  tg.close()
+}
+
 function App() {
   return (
     <div className="main">
+        <button type='button' onClick={onClose}>Close</button>
         <h1>Здесь про Гороскопы</h1>
         <Zodiacs/>
     </div>
--- src/components/Zodiacs.jsx
+++ src/components/Zodiacs.jsx
@@ -15,73 +15,73 @@ import Sagittarius from '../svg/sagittarius-svgrepo-com.jsx';
 
 const Zodiacs = (props) => (
     <div className="signs">
-        <Link to="/Horoscope" state={{sign: 'aries', name: 'Овен'}}>
+        <Link to="/Horoscope" className="zodiac_box" state={{sign: 'aries', name: 'Овен'}}>
         <h3>Овен</h3>
         <Aries className="zodiac_sign"/>
         <p>21 марта - 20&nbsp;апреля</p>
         </Link>
 
-        <Link to="/Horoscope" state={{sign: 'taurus', name: 'Телец'}}>
+        <Link to="/Horoscope" className="zodiac_box" state={{sign: 'taurus', name: 'Телец'}}>
         <h3>Телец</h3>
         <Taurus className="zodiac_sign"/>
         <p>21 апреля - 20&nbsp;мая</p>
         </Link>
 
-        <Link to="/Horoscope" state={{sign: 'gemini', name: 'Близнецы'}}>
+        <Link to="/Horoscope" className="zodiac_box" state={{sign: 'gemini', name: 'Близнецы'}}>
         <h3>Близнецы</h3>
         <Gemini className="zodiac_sign"/>
         <p>21 мая - 21&nbsp;июня</p>
         </Link>
 
-        <Link to="/Horoscope" state={{sign: 'cancer', name: 'Рак'}}>
+        <Link to="/Horoscope" className="zodiac_box" state={{sign: 'cancer', name: 'Рак'}}>
         <h3>Рак</h3>
         <Cancer className="zodiac_sign"/>
         <p>22 июня - 22&nbsp;июля</p>
         </Link>
 
-        <Link to="/Horoscope" state={{sign: 'leo', name: 'Лев'}}>
+        <Link to="/Horoscope" className="zodiac_box" state={{sign: 'leo', name: 'Лев'}}>
         <h3>Лев</h3>
         <Leo className="zodiac_sign"/>
         <p>23 июля - 23&nbsp;августа</p>
         </Link>
 
-        <Link to="/Horoscope" state={{sign: 'virgo', name: 'Дева'}}>
+        <Link to="/Horoscope" className="zodiac_box" state={{sign: 'virgo', name: 'Дева'}}>
         <h3>Дева</h3>
         <Virgo className="zodiac_sign"/>
         <p>24 августа - 23&nbsp;сентября</p>
         </Link>
 
-        <Link to="/Horoscope" state={{sign: 'libra', name: 'Весы'}}>
+        <Link to="/Horoscope" className="zodiac_box" state={{sign: 'libra', name: 'Весы'}}>
         <h3>Весы</h3>
         <Libra className="zodiac_sign"/>
         <p>24 сентября - 23&nbsp;октября</p>
         </Link>
 
-        <Link to="/Horoscope" state={{sign: 'scorpio', name: 'Скорпион'}}>
+        <Link to="/Horoscope" className="zodiac_box" state={{sign: 'scorpio', name: 'Скорпион'}}>
         <h3>Скорпион</h3>
         <Scorpio className="zodiac_sign"/>
         <p>24 октября - 22&nbsp;ноября</p>
         </Link>
 
-        <Link to="/Horoscope" state={{sign: 'sagittarius', name: 'Стрелец'}}>
+        <Link to="/Horoscope" className="zodiac_box" state={{sign: 'sagittarius', name: 'Стрелец'}}>
         <h3>Стрелец</h3>
         <Sagittarius className="zodiac_sign"/>
         <p>23 ноября - 21&nbsp;декабря</p>
         </Link>
 
-        <Link to="/Horoscope" state={{sign: 'capricorn', name: 'Козерог'}}>
+        <Link to="/Horoscope" className="zodiac_box" state={{sign: 'capricorn', name: 'Козерог'}}>
         <h3>Козерог</h3>
         <Capricorn className="zodiac_sign"/>
         <p>22 декабря - 20&nbsp;января</p>
         </Link>
 
-        <Link to="/Horoscope" state={{sign: 'aquarius', name: 'Водолей'}}>
+        <Link to="/Horoscope" className="zodiac_box" state={{sign: 'aquarius', name: 'Водолей'}}>
         <h3>Водолей</h3>
         <Aquarius className="zodiac_sign"/>
         <p>21 января - 20&nbsp;февраля</p>
         </Link>
 
-        <Link to="/Horoscope" state={{sign: 'pisces', name: 'Рыбы'}}>
+        <Link to="/Horoscope" className="zodiac_box" state={{sign: 'pisces', name: 'Рыбы'}}>
         <h3>Рыбы</h3>
         <Pisces className="zodiac_sign"/>
         <p>21 февраля - 20&nbsp;марта</p>
