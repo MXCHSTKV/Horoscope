fe1638c45904cca70e1034baa66d8a843249c00b
Max Chistiakov <chistiakovmax@gmail.com>
2024-10-14 01:02:15 +0200

    fixed API error

--- src/components/Signs.jsx
+++ src/components/Signs.jsx
@@ -8,7 +8,9 @@ translate.key = process.env.REACT_APP_DEEPL_KEY
 
 const Signs = () => {
     const [data, setData] = useState('Загрузка...');
+    const [dataWeekly, setDataWeekly] = useState('Загрузка...');
     const [ruData, setRuData] = useState('Data not loaded')
+    const [ruDataWeekly, setRuDataWeekly] = useState('Data not loaded')
     const location = useLocation();
     const {sign, name, ru, date} = location.state
 
@@ -18,8 +20,22 @@ const Signs = () => {
                 const response = await axios.get(`https://pure-server.onrender.com/horoscope?sign=${sign}&date=${date}`);
                 setData(response.data);
             } catch (error) {
-                console.error('Ошибка запроса', error)
-                setData('Ошибка загрузки данных')
+                console.error(ru ? 'Ошибка запроса' : 'Request error', error)
+                setData(ru ? 'Ошибка загрузки данных' : 'Error loading data')
+            }
+        };
+
+        fetchData(sign, date);
+    }, [sign]);
+
+    useEffect(() => {
+        const fetchData = async (sign) => {
+            try {
+                const response = await axios.get(`https://pure-server.onrender.com/horoscope/weekly?sign=${sign}`);
+                setDataWeekly(response.data);
+            } catch (error) {
+                console.error(ru ? 'Ошибка запроса' : 'Request error', error)
+                setDataWeekly(ru ? 'Ошибка загрузки данных' : 'Error loading data')
             }
         };
 
@@ -29,16 +45,22 @@ const Signs = () => {
     useEffect(()=>{
         const translateData = async () => {
             const translatedData = await translate(data, 'ru')
+            const translatedDataWeekly = await translate(dataWeekly, 'ru')
+
             setRuData(translatedData)
+            setRuDataWeekly(translatedDataWeekly)
         }
         translateData()
-    }, [data])
+    }, [data, dataWeekly])
 
     return (
         <div className='sign'>
             <h1>{name}</h1>
             <h3 className='sign_h3'>{ru ? 'Твой гороскоп на сегодня:' : 'Your horoscope for today:'}</h3>
             <p className='sign'>{ru ? ruData : data}</p>
+            <h3 className='sign_h3'>{ru ? 'Твой гороскоп на неделю:' : 'Your horoscope for week:'}</h3>
+            <p className='sign'>{ru ? ruDataWeekly : dataWeekly}</p>
+
         </div>
     );
 };
